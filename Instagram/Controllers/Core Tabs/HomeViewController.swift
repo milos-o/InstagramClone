ebfea0e2d3ba169d039f00214a556eab0f7a9d5c
Miloš Ostojić <milos.osto11@gmail.com>
2021-08-16 11:52:54 +0200

    first commit

---
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Amplitudo on 16.8.21..
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
